7413b660ccdda7a4b15e29a301f3e75e1a6eb174
efdubya4 <90352730+efdubya4@users.noreply.github.com>
2025-04-08 17:47:36 -0400

    fix config-override

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,29 +1,28 @@
 const webpack = require("webpack");
 
-module.exports = function override(config){
-    const fallback = config.resolve.fallback || {};
+module.exports = function override(config) {
+    config.resolve = {
+        ...config.resolve,
+        fallback: {
+            zlib: require.resolve("browserify-zlib"),
+            querystring: require.resolve("querystring-es3"),
+            path: require.resolve("path-browserify"),
+            fs: false,
+            crypto: require.resolve("crypto-browserify"),
+            stream: require.resolve("stream-browserify"),
+            http: require.resolve("stream-http"),
+            net: false,
+            module: false,
+            dns: false,
+            tls: false,
+            vm: require.resolve("vm-browserify"),
+        },
+    };
 
-    Object.assign(fallback, {
-        zlib: require.resolve("browserify-zlib"),
-        querystring: require.resolve("querystring-es3"),
-        path: require.resolve("path-browserify"),
-        fs: false,
-        crypto: require.resolve("crypto-browserify"),
-        stream: require.resolve("stream-browserify"),
-        http: require.resolve("stream-http"),
-        net: false,
-        module: false, 
-        dns: false,
-        tls: false,
-        'process/browser': require.resolve("process/browser"),
-        buffer: require.resolve("buffer"),
-        vm: require.resolve("vm-browserify"),
-    });
-    config.resolve.fallback = fallback;
     config.plugins = (config.plugins || []).concat([
         new webpack.ProvidePlugin({
-            process:'process/browser',
-            Buffer: ['buffer', 'Buffer'],
+            process: "process",
+            Buffer: ["buffer", "Buffer"],
         }),
     ]);
 
